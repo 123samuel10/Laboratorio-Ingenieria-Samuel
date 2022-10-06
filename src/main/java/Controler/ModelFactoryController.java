5da48125d7e498ab2cef578635045a3858c24381
samuel <samuelsalcedoramirez@gmail.com>
2022-10-05 19:26:26 -0500

    Carpetas

--- src/main/java/Controler/ModelFactoryController.java
+++ src/main/java/Controler/ModelFactoryController.java
@@ -3,6 +3,8 @@ package Controler;
 import service.ModelFactoryControllerService;
 
 public class ModelFactoryController implements ModelFactoryControllerService {
+
+
     private static class SingletonHolder {
         // El constructor de Singleton puede ser llamado desde aquí al ser protected
         private final static ModelFactoryController eINSTANCE = new ModelFactoryController();
@@ -16,5 +18,10 @@ public class ModelFactoryController implements ModelFactoryControllerService {
     public ModelFactoryController() {
         System.out.println("invocación clase singleton");
     //    laboratorio = new Laboratorio();
+    }
+
+    @Override
+    public void agregarEstudiante(String nombre, String id, String carrera, String telefono, String correo) {
+
     }
 }
--- src/main/java/service/ModelFactoryControllerService.java
+++ src/main/java/service/ModelFactoryControllerService.java
@@ -1,4 +1,5 @@
 package service;
 
 public interface ModelFactoryControllerService {
+    void agregarEstudiante(String nombre,String id,String carrera,String telefono,String correo);
 }
